f1d228d09a773bb7fdd7e556087d2f9b6bd195ef
abhinav031-tt <113112279+abhinav031-tt@users.noreply.github.com>
2022-10-13 23:31:10 +0530

    Add files via upload

---
+++ challenge_4_.py
@@ -0,0 +1,16 @@
+class account ():
+ def __init__(self, name , balance = 0) :
+       self.name = name
+       self.balance = 0
+
+def show_detail (self) :
+    print ("Personal detail")
+    print("")    
+    print("name", self.name)
+
+
+class savingsAccount():
+ 
+    def __init__(self, name , balance = 0):
+      super().__init__(name, )
+      self.balance = 0
